b6cd3a5d25014ccfcc9e27d2c32f75156b337b1d
codershani <codershani13@gmail.com>
2024-09-29 19:29:55 +0530

    feat: Health endpoint -> know system and application health

--- src/controllers/api-controller.ts
+++ src/controllers/api-controller.ts
@@ -2,11 +2,32 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpError, HttpResponse } from '../utils/common';
 import StatusCodes from 'http-status-codes';
 import { ResponseMessage } from '../utils/constants';
+import { Quicker } from '../utils/helper';
 
 class ApiController {
     public static self(req: Request, res: Response, next: NextFunction) {
         try {
             HttpResponse(req, res, StatusCodes.OK, ResponseMessage.SUCCESS);
+        } catch (error) {
+            HttpError(next, error, req, StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public static health(req: Request, res: Response, next: NextFunction) {
+        try {
+            const healthData = {
+                application: Quicker.getApplicationHealth(),
+                system: Quicker.getSystemHealth(),
+                timestamp: Date.now(),
+            };
+
+            HttpResponse(
+                req,
+                res,
+                StatusCodes.OK,
+                ResponseMessage.SUCCESS,
+                healthData,
+            );
         } catch (error) {
             HttpError(next, error, req, StatusCodes.INTERNAL_SERVER_ERROR);
         }
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,5 +4,6 @@ import ApiController from '../../controllers/api-controller';
 const router = express.Router();
 
 router.route('/self').get(ApiController.self);
+router.route('/health').get(ApiController.health);
 
 export default router;
