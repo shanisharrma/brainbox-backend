d7162e42a2f5fd965286d89a068c68f6931e0c2f
codershani <codershani13@gmail.com>
2024-11-05 15:00:01 +0530

    fix: Sequelize setup

--- src/database/sequelize.config.js
+++ src/database/sequelize.config.js
@@ -24,5 +24,8 @@ module.exports = {
         host: ServerConfig.DB_HOST,
         port: ServerConfig.DB_PORT,
         dialect: 'mysql',
+        dialectOptions: {
+            ssl: true, // This can be required for secure connections
+        },
     },
 };
--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -6,7 +6,11 @@ const connection: Sequelize = new Sequelize(
     ServerConfig.DB_PASS ?? '',
     {
         host: ServerConfig.DB_HOST ?? '',
+        port: Number(ServerConfig.DB_PORT) || 3306,
         dialect: 'mysql',
+        dialectOptions: {
+            ssl: true, // Some hosting services require SSL; Render may require this based on your database settings.
+        },
     },
 );
 export default connection;
